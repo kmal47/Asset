9249260a2fa72a657f9cc722468d3971c5dbe0e0
akmal <akmal.sharif4747@gmail.com>
2018-06-01 10:59:38 +0800

    1st

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{config('app.name','AssetManagement')}}</title>
+
+    </head>
+    <body>
+    <h1>Home</h1>
+    </body>
+</html>
